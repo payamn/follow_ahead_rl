588d92cb9a3dbaaccb9b157ef0ffb61327a6ee7f
payam nikdel <payam222@gmail.com>
2019-08-01 19:40:01 -0700

    now can create two robot one is person recording path and following the path recorded working still need to complete get reward

--- gym-webots/gym_webots/envs/gym_webots.py
+++ gym-webots/gym_webots/envs/gym_webots.py
@@ -15,6 +15,7 @@ from squaternion import quat2euler
 
 from sensor_msgs.msg import NavSatFix
 from sensor_msgs.msg import Imu
+from sensor_msgs.msg import LaserScan
 
 from webots_ros.srv import get_bool
 from webots_ros.srv import set_int
@@ -30,6 +31,8 @@ from webots_ros.srv import field_import_node_from_string
 from webots_ros.srv import field_import_node_from_stringRequest
 
 from simple_pid import PID
+
+import pickle
 
 import threading
 
@@ -66,8 +69,13 @@ class Supervisor:
     def __init__(self):
         self.root_service_str = '/manager/supervisor/'
 
-        self.str_robot_create = ' controller "ros" \
-                                        controllerArgs "--name=pioneer3at" extensionSlot [ Camera { translation 0 0.17 -0.22\
+        self.services = {}
+        self.init_services()
+
+    def get_create_robot_str(self, name, model, translation, rotation):
+        robot_str = 'DEF ' + name + ' ' + model + ' { translation ' + translation + ' rotation ' + rotation + \
+                                        ' controller "ros" \
+                                        controllerArgs "--name=' + name + '" extensionSlot [ Camera { translation 0 0.17 -0.22\
                                         width 256 height 128 motionBlur 500 noise 0.02}\
                                         Accelerometer { lookupTable [ -39.24 -39.24 0.005 39.24 39.24 0.005 ]}\
                                         Gyro {lookupTable [-50 -50 0.005 50 50 0.005 ]}\
@@ -75,9 +83,7 @@ class Supervisor:
                                         GPS {}\
                                         InertialUnit {}\
                                         ]}'
-
-        self.services = {}
-        self.init_services()
+        return robot_str
 
     def init_services(self):
         self.services['create_object'] = Service(self.root_service_str + "field/import_node_from_string",
@@ -114,7 +120,7 @@ class Supervisor:
 
     def create_robot(self, name, model, translation, rotation):
         self.remove_object(defName=name)
-        robot_str = "DEF " + name + " " + model + " { translation " + translation + " rotation " + rotation + self.str_robot_create
+        robot_str = self.get_create_robot_str(name, model, translation, rotation)
         self.services['create_object'].call(field_import_node_from_stringRequest(field=self.child_field, position=-1, nodeString=robot_str))
 
 
@@ -126,27 +132,67 @@ class Robot():
     def __init__(self, name, init_pos, angle, supervisor, model='Pioneer3at'):
         self.model = 'Pioneer3at'
         self.name = name
-        self.robot_service_str = '/pioneer3at/'
+        self.robot_service_str = '/{}/'.format(self.name)
         self.supervisor = supervisor
-        self.supervisor.create_robot(self.name, self.model, str(init_pos[0]) + " 0.178 " + str(init_pos[1]), "0 1 0 "+str(angle))
+
+        self.supervisor.create_robot(self.name, self.model, str(-init_pos[1]) + " 0.178 " + str(-init_pos[0]), "0 1 0 "+str(angle))
         self.wheels_left = ['back_left_wheel', 'front_left_wheel']
         self.wheels_right = ['back_right_wheel', 'front_right_wheel']
         self.services = {}
         self.max_speed = 6.3
+        self.max_laser_range = 25.0 # meter
+        self.width_laser_image = 100
+        self.height_laser_image = 50
         self.pos = (0, 0)
         self.init_services()
-        self.angular_pid = PID(0.25, 0, 0.74, setpoint=0)
+        self.angular_pid = PID(0.45, 0, 0.74, setpoint=0)
         self.linear_pid = PID(4, 0, 0.05, setpoint=0)
         self.orientation = angle
         self.pos_sub = rospy.Subscriber(self.robot_service_str+'/gps/values', NavSatFix, self.position_cb)
         self.imu_sub = rospy.Subscriber(self.robot_service_str+'/inertial_unit/roll_pitch_yaw', Imu, self.imu_cb)
+        self.laser_sub = rospy.Subscriber(self.robot_service_str+'/Sick_LMS_291/laser_scan/layer0', LaserScan, self.laser_cb)
 
+    def take_action(self, action):
+        speed_left = 0
+        speed_right = 0
+        if action == 0:
+            pass # stop robot
+        elif action == 1:
+            speed_left = speed_right = self.max_speed
+        elif action == 2:
+            speed_left = speed_right = -self.max_speed
+        elif action == 3:
+            speed_left = self.max_speed
+            speed_right = -self.max_speed
+        elif action == 4:
+            speed_left = -self.max_speed
+            speed_right = self.max_speed
+        elif action == 5:
+            speed_left = self.max_speed/2
+            speed_right = self.max_speed/2
+        elif action == 6:
+            speed_left = self.max_speed/2
+            speed_right = -self.max_speed/2
+        elif action == 7:
+            speed_left = -self.max_speed/2
+            speed_right = self.max_speed/2
+
+        for wheel in self.wheels_left:
+            self.services[wheel + "_vel"].call(speed_left)
+        for wheel in self.wheels_right:
+            self.services[wheel + "_vel"].call(speed_right)
+
+    def stop_robot(self):
+        for wheel in self.wheels_left:
+            self.services[wheel + "_vel"].call(0)
+        for wheel in self.wheels_right:
+            self.services[wheel + "_vel"].call(0)
 
     def go_to_pos(self, pos):
         angle = math.atan2(pos[1]-self.pos[1], pos[0]-self.pos[0])
         distance = math.hypot(pos[0]-self.pos[0], pos[1]-self.pos[1])
         # diff_angle_prev = (angle - self.orientation + math.pi) % (math.pi * 2) - math.pi
-        while not distance < 0.5:
+        while not distance < 1.5:
             angle = math.atan2(pos[1] - self.pos[1], pos[0] - self.pos[0])
             distance = math.hypot(pos[0] - self.pos[0], pos[1] - self.pos[1])
             diff_angle = (angle -self.orientation + math.pi) % (math.pi*2) - math.pi
@@ -169,8 +215,8 @@ class Robot():
                 self.services[wheel+"_vel"].call(left_vel)
             for wheel in self.wheels_right:
                 self.services[wheel+"_vel"].call(right_vel)
-            rospy.loginfo("angle: {} distance: {} vel: angular: {} linear: {} left: {} right: {} diff: {} orientation: {}"\
-                .format(np.rad2deg(angle), distance, angular_vel, linear_vel, left_vel, right_vel, np.rad2deg(diff_angle), np.rad2deg(self.orientation)))
+            # rospy.loginfo("angle: {} distance: {} vel: angular: {} linear: {} left: {} right: {} diff: {} orientation: {}"\
+            #     .format(np.rad2deg(angle), distance, angular_vel, linear_vel, left_vel, right_vel, np.rad2deg(diff_angle), np.rad2deg(self.orientation)))
             time.sleep(0.1)
 
     def get_pos(self):
@@ -180,9 +226,28 @@ class Robot():
         self.orientation = quat2euler(
             imo_msg.orientation.x, imo_msg.orientation.y, imo_msg.orientation.z, imo_msg.orientation.w)[0]
 
+    def laser_cb(self, laser_msg):
+        angle_increments = np.arange(float(laser_msg.angle_min) - float(laser_msg.angle_min) , float(laser_msg.angle_max) - 0.001 - float(laser_msg.angle_min), float(laser_msg.angle_increment))
+        ranges = np.asarray(laser_msg.ranges, dtype=float)
+        remove_index = np.append(np.argwhere(ranges >= self.max_laser_range), np.argwhere(ranges <= laser_msg.range_min))
+        angle_increments = np.delete(angle_increments, remove_index)
+        ranges = np.delete(ranges, remove_index)
+
+        x = np.floor((self.width_laser_image/2.0 - (self.height_laser_image / self.max_laser_range) * np.multiply(np.cos(angle_increments), ranges))).astype(np.int)
+        y = np.floor((self.height_laser_image-1 - (self.height_laser_image / self.max_laser_range) * np.multiply(np.sin(angle_increments), ranges))).astype(np.int)
+        if len(x) > 0 and (np.max(x) >= self.width_laser_image or np.max(y) >= self.height_laser_image):
+            print ("problem, max x:{} max y:{}".format(np.max(x), np.max(y)))
+        scan_image = np.zeros((self.height_laser_image, self.width_laser_image), dtype=np.uint8)
+        scan_image[y, x] = 255
+        self.scan_image = scan_image
+        if "robot" in self.name:
+            cv.namedWindow("laser {}".format(self.name), cv.WINDOW_AUTOSIZE)
+            cv.imshow("laser {}".format(self.name), scan_image)
+            cv.waitKey(1)
+
     def position_cb(self, pos_msg):
         self.pos = (-pos_msg.longitude, -pos_msg.latitude)
-        rospy.loginfo_throttle(0.2, "current pos: {}, orientation {}".format(self.pos, self.orientation))
+        # rospy.loginfo_throttle(0.2, "current pos: {}, orientation {}".format(self.pos, self.orientation))
 
     def init_services(self):
         self.services = {}
@@ -213,70 +278,86 @@ class Robot():
 
 class WebotsEnv(gym.Env):
 
-    def remove_robot(self):
-        self.supervisor.remove_object('my_robot')
+    def calculate_angle_using_path(self, idx):
+        return math.atan2(self.path[idx+1][1] - self.path[idx][1], self.path[idx+1][0] - self.path[idx][0])
+
+    def get_person_position_relative_robot(self):
+        return np.asarray(self.person.pos) - np.asarray(self.robot.pos)
+
+    def path_follower(self, robot):
+        if len(self.path)==0:
+            return
+        for idx, point in enumerate(self.path[1:]):
+            robot.go_to_pos(point)
+            rospy.loginfo("got to point: {} out of {}".format(idx, len(self.path)))
+        robot.stop_robot()
+
+    def get_laser_scan(self):
+        return self.robot.scan_image
+
+    def get_observation(self):
+        orientation_person = np.asarray(self.person.orientation)
+        orientation_robot = np.asarray(self.robot.orientation)
+
+        orientation_position = np.append(orientation_person, orientation_robot, self.get_person_position_relative_robot())
+
+        return self.get_laser_scan(), orientation_position
 
     def __init__(self):
-        self.node = rospy.  init_node("webots_env", anonymous=True)
+        self.node = rospy.init_node("webots_env", anonymous=True)
 
         self.supervisor = Supervisor()
+        # read the path for robot
+        self.path = []
+        try:
+            with open('data/first', 'rb') as f:
+                self.path = pickle.load(f)
+        except Exception as e:
+            print(e)
 
+        idx_start = 0
+        init_pos_person = self.path[idx_start]
+        angle_person = self.calculate_angle_using_path(idx_start)
+        angle_robot = self.calculate_angle_using_path(idx_start+5)
+        self.person = Robot('person', init_pos=self.path[idx_start],  angle=angle_person, supervisor=self.supervisor)
+        self.robot = Robot('my_robot', init_pos=self.path[idx_start+5],  angle=angle_robot, supervisor=self.supervisor)
+        self.path = self.path[idx_start+5:]
+        _thread.start_new_thread(self.path_follower, (self.person,))
 
-        self.robot = Robot('my_robot', init_pos=(1,1),  angle=np.deg2rad(0), supervisor=self.supervisor)
-        _thread.start_new_thread(self.robot.go_to_pos, ((-14, 0),) )
         rospy.spin()
-        del self.robot
-        return
-        x.call(True)
-
-        self.reset_service_str = self.manager_service_str +'simulation_reset'
-        self.gps_service_str = self.robot_service_str + 'gps/enable'
-
-        rospy.wait_for_service(self.reset_service_str)
-        self.reset_srv = rospy.ServiceProxy(self.reset_service_str, get_bool)
-        self.reset_srv.call(True)
-
         self.observation_space = gym.spaces.Tuple(
                 (
-                    gym.spaces.Box(low=0, high=1, shape=(100, 100)),
+                    gym.spaces.Box(low=0, high=1, shape=(50, 100)),
                     gym.spaces.Box(low=0, high=1, shape=(8,))
                 )
             )
         # Action space omits the Tackle/Catch actions, which are useful
         # on defense
-        self.action_space = gym.spaces.Discrete(self.env.get_action_space())
+        self.action_space = gym.spaces.Discrete(8)
         self.min_distance = 1
         self.max_distance = 3.5
         self.number_of_steps = 0
         self.max_numb_steps = 40
         self.distance = 0
         self.reward_range = [-1, 1]
-        self._person_rand_thread = threading.Thread(
-            target=self.person_random_point, args=(), daemon=True)
-        self._person_rand_thread.start()
-
-
-
-    def person_random_point(self):
-        while (True):
-            time.sleep(.1)
 
     def __del__(self):
         # todo
         return
 
     def take_action(self, action):
-        # todo
+        self.robot.take_action(action)
         return
+
     def step(self, action):
         self.number_of_steps += 1
-        self.env.take_action(action)
+        self.take_action(action)
         reward = self.get_reward()
-        ob = self.env.get_observation()
+        ob = self.get_observation()
         episode_over = False
-        rel_person = self.env.get_person_position_relative_robot()
+        rel_person = self.get_person_position_relative_robot()
         self.distance = math.hypot(rel_person[0], rel_person[1])
-        if self.env.is_collided():
+        if self.distance < 0.5:
             episode_over = True
             print('collision happened episode over')
             reward -= 1
@@ -290,7 +371,7 @@ class WebotsEnv(gym.Env):
         return ob, reward, episode_over, {}
 
     def get_reward(self):
-        pos_rel = self.env.get_person_position_relative_robot()
+        pos_rel = self.get_person_position_relative_robot()
         reward = 0
         distance = math.hypot(pos_rel[0], pos_rel[1])
         angle_robot_person = self.env.get_angle_person_robot()
---
+++ scripts/record_path.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2 as cv
+
+import rospy
+import math
+
+from sensor_msgs.msg import NavSatFix
+
+import subprocess
+import pickle
+
+robot_service_str = '/pioneer3at/'
+all_pos = []
+def position_cb(pos_msg):
+    pos = (-pos_msg.longitude, -pos_msg.latitude)
+    if len(all_pos) == 0 or math.hypot(all_pos[-1][0]-pos[0], all_pos[-1][1]-pos[1]) > 0.5:
+        all_pos.append(pos)
+if __name__=="__main__":
+    rospy.init_node("record_path", anonymous=True)
+    subprocess.call(["mkdir", "data", "-p"])
+    pos_sub = rospy.Subscriber(robot_service_str + '/gps/values', NavSatFix, position_cb)
+    val = input("Start saving path when finished please enter with name of the file: ")
+    with open('data/'+val, 'wb') as f:
+        pickle.dump(all_pos, f)
