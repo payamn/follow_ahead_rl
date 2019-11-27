d59f5d22aa5d76badff9cb01e131b0fc2e849838
Payam <payam222@gmail.com>
2019-11-27 08:07:24 -0800

    fixed param

--- scripts/dp_value_based.py
+++ scripts/dp_value_based.py
@@ -24,22 +24,23 @@ class DpValueBased:
 
     def __init__(self):
         self.action_space = 9
-        self.Q = np.zeros((100, 100, 72, self.action_space))
-        self.V = np.zeros((100, 100, 72))
-        self.time_interval = 0.3
+        self.Q = np.zeros((50, 50, 36, self.action_space))
+        self.V = np.zeros((50, 50, 36))
+        self.time_interval = 0.2
         self.person_velocity = (0.5, 0.1)
         self.use_v = True
         self.depth = 2  # td depth lambda
         self.gamma = 0.8
-        self.alpha = 0.015
+        self.alpha = 0.005
         self.max_num_episodes = 10000000
         self.save_lock = threading.Lock()
         self.initialize_v_with_reward()
-        # for i in range (20):
-        #     start = time.time()
-        #     self.update_v()
-        #     print ("time: {}".format(time.time()-start))
-        #     self.save_threaded()
+        number_of_run = 500
+        for i in range (number_of_run):
+            start = time.time()
+            self.update_v()
+            print("time to finish: min {} hours {} i: {}".format(((number_of_run-i) * (time.time()-start))/60, ((number_of_run-i) * (time.time()-start))/60/60, i))
+            self.save_threaded()
         self.visualize_v()
         #self.load_q()
         # self.visualize()
@@ -106,32 +107,35 @@ class DpValueBased:
             print ("loading q")
             self.Q = np.load("q.npy")
 
-    def get_best_action(self, heading, pos):
+    def get_best_action(self, heading, pos, current_velocity):
         x_index, y_index, degree_index = self.get_index_from_heading_pos(heading, pos)
         if self.use_v:
-            reward, action = self.depth_recursive_reward(pos[0], pos[1], heading, self.depth, get_action=True, use_random_speed_person=False)
+            reward, action = self.depth_recursive_reward(pos[0], pos[1], heading, self.depth, current_velocity, get_action=True, use_random_speed_person=False)
         else:
             actions = self.Q[x_index, y_index, degree_index]
         return action
 
     def get_robot_state_from_table(self,x, y, a):
-        heading = a * 2 * math.pi / 72.
-        x1 = (x - 50)/10.
-        x2 = (y - 50)/10.
+        heading = a * 2 * math.pi / 36.
+        x1 = (x - self.V.shape[0]/2)/5.
+        x2 = (y - self.V.shape[1]/2)/5.
         return x1, x2, heading
 
     def get_index_from_heading_pos(self, heading, pos):
         heading = DpValueBased.zero_2pi(heading)
         degree = np.rad2deg(heading)
-        degree_index = int(degree * 72 / 360.)
+        degree_index = math.floor(degree * 36 / 360.)
         x = min(max(pos[0], -5), 5) + 5
-        x_index = int(x * 10)
+        x_index = math.floor(x * self.V.shape[0]/2/5)
         y = min(max(pos[1], -5), 5) + 5
-        y_index = int(y * 10)
+        y_index = math.floor(y *  self.V.shape[0]/2/5)
 
-        x_index = min(max(x_index, 0), 99)
-        y_index = min(max(y_index, 0), 99)
-        degree_index = min(max(degree_index, 0), 71)
+        x_index = min(max(x_index, 0), self.V.shape[0]-1)
+        y_index = min(max(y_index, 0), self.V.shape[1]-1)
+        degree_index_before = degree_index
+        degree_index = min(max(degree_index, 0), self.V.shape[2]-1)
+        if abs(degree_index - degree_index_before) > 1:
+            print ("degree index may be wront: prev, now: {} {}".format(degree_index_before, degree_index))
         return (x_index, y_index, degree_index)
 
     def set_q_value(self, q_value, heading, pos, action):
@@ -143,7 +147,7 @@ class DpValueBased:
 
         return self.Q[x_index, y_index, degree_index, action]
 
-    def action_angular_linear(self, action):
+    def action_angular_linear(self, action, current_velocity=(None,None)):
         angular = 0
         linear = 0
         if action == 1:
@@ -166,7 +170,9 @@ class DpValueBased:
         elif action ==8:
             angular = -0.5
             linear = 1
-
+        if current_velocity[0] != None:
+            angular = (angular + current_velocity[1])/2.
+            linear = (linear + current_velocity[0])/2.
         return angular, linear
 
     def is_collided (self, state):
@@ -190,13 +196,13 @@ class DpValueBased:
         elif abs(distance - 1.7) < 0.7:
             reward += 0.1 * (0.7 - abs(distance - 1.7))
         elif distance >= 1.7:
-            reward -= 0.01 * distance
+            reward -= 0.16 * (distance-1.7)
         elif distance < 1:
-            reward -= 0.7 - distance
+            reward -= (1 - distance) * 2
         if abs(angle_robot_person) < 45:
-            reward += 0.2 * (45 - abs(angle_robot_person)) / 45
+            reward += 0.20 * (45 - abs(angle_robot_person)) / 45
         else:
-            reward -= 0.3 * abs(angle_robot_person) / 180
+            reward -= 0.80 * (abs(angle_robot_person) - 45) / 180
         # if not 90 > angle_robot_person > 0:
         #     reward -= distance/6.0
         # elif self.min_distance < distance < self.max_distance:
@@ -220,16 +226,18 @@ class DpValueBased:
                     reward = self.calculate_reward((x1, x2, heading))
                     self.V[x, y, a] = reward
 
-    def depth_recursive_reward(self, x, y, a, depth, get_action=False, use_random_speed_person=True):
+    def depth_recursive_reward(self, x, y, a, depth, current_velocity, get_action=False, use_random_speed_person=False):
         if depth <= 0:
-            return 0
+            xi_1, xi_2, xi_3  = self.get_index_from_heading_pos(a, (x,y))
+            return self.V[xi_1, xi_2, xi_3]
         if self.is_collided((x,y,a)):
             return -1
-        x1, x2, heading = self.get_index_from_heading_pos(a, (x,y))
-        state_reward = self.V[x1, x2, heading]
+        # xi_1, xi_2, xi_3  = self.get_index_from_heading_pos(a, (x,y))
+        state_reward = self.calculate_reward((x, y, a))
         reward_list = []
         for action in range(9):
-            angular, linear = self.action_angular_linear(action)
+            angular, linear = self.action_angular_linear(action, current_velocity)
+            current_velocity = (linear, angular)
             if use_random_speed_person:
                 person_angular = random.random() - 0.5
                 person_linear = random.random() - 0.5
@@ -237,7 +245,7 @@ class DpValueBased:
                 person_angular = self.person_velocity[1]
                 person_linear = self.person_velocity[0]
             x1, x2, x3 = self.model(person_linear, linear, person_angular, angular, x, y, a, self.time_interval)
-            reward_r = self.depth_recursive_reward(x1, x2, x3, depth-1, use_random_speed_person=use_random_speed_person)
+            reward_r = self.depth_recursive_reward(x1, x2, x3, depth-1, current_velocity, use_random_speed_person=use_random_speed_person)
             reward = state_reward + self.gamma * reward_r
             reward_list.append(reward)
         if get_action:
@@ -245,10 +253,10 @@ class DpValueBased:
             return max(reward_list), np.argmax(reward_list)
         return max(reward_list)
 
-    def update_v_thread(self, x, y, a_start, a_end):
+    def update_v_thread(self, x, y, a_start, a_end, current_velocity):
         for a in range(a_start, a_end):
             x1, x2, heading = self.get_robot_state_from_table(x, y, a)
-            reward = self.depth_recursive_reward(x1, x2, heading, self.depth, use_random_speed_person=False)
+            reward = self.depth_recursive_reward(x1, x2, heading, self.depth, current_velocity, use_random_speed_person=False)
             with self.save_lock:
                 self.V[x, y, a] = self.V[x, y, a] + self.alpha * (reward - self.V[x, y, a])
 
@@ -257,7 +265,8 @@ class DpValueBased:
             for y in range (self.V.shape[1]):
                 len_a = self.V.shape[2]
                 num_thread = 18
-                threads = [threading.Thread(target=self.update_v_thread, args=(x, y, i*math.ceil(72./num_thread), min((i+1)*math.ceil(72./num_thread), 72))) for i in range(num_thread)]
+                current_velocity_robot = (0,0)
+                threads = [threading.Thread(target=self.update_v_thread, args=(x, y, i*math.ceil(36./num_thread), min((i+1)*math.ceil(36./num_thread), 36), current_velocity_robot)) for i in range(num_thread)]
                 for t in threads:
                     t.start()
                 for t in threads:
@@ -314,18 +323,17 @@ class DpValueBased:
                 if random.random() > 1:
                     action = random.randint(0, self.action_space-1)
                 else:
-                    action = self.get_best_action(heading, pose)
+                    action = self.get_best_action(heading, pose, robot_velocity)
 
                 angular, linear = self.action_angular_linear(action)
                 # angular = -0.5
                 # linear = 0.5
-                print("before step")
                 observation, reward, over, _ = env.step((linear, angular))
-                # x = DpValueBased.model(person_velocity[0], robot_velocity[0], person_velocity[1], robot_velocity[1], pose[0], pose[1], heading, 0.077)
+                x = DpValueBased.model(person_velocity[0], robot_velocity[0], person_velocity[1], robot_velocity[1], pose[0], pose[1], heading, 0.077)
+
+                print("estimated vs real: x1:{} {} x2:{} {} x3:{} {}".format(x[0], pose[0], x[1], pose[1], x[2], DpValueBased.zero_2pi(heading)))
                 heading, pose, person_velocity, robot_velocity = observation
                 self.person_velocity = person_velocity
-                # print("estimated vs real: x1:{} {} x2:{} {} x3:{} {}".format(x[0], pose[0], x[1], pose[1], x[2], DpValueBased.zero_2pi(heading)))
-
                 obs_reward.append((observation, reward, over, action))
                 rewards.append(reward)
                 # if len(obs_reward) > self.depth:
@@ -348,7 +356,6 @@ class DpValueBased:
             #     else:
             #         self.save_threaded("q")
             #wandb.log({"reward": np.average(rewards)})
-            print("before reset {}".format(num_episodes))
             env.reset()
 
 if __name__ == '__main__':
