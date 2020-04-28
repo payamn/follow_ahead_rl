e75571b9027621ccf24af70520e07935f6937d7d
Payam <payam222@gmail.com>
2020-04-27 17:00:52 -0700

    removed d4pg to use the git repo

--- scripts/d4pg-pytorch/train.py
+++
@@ -1,9 +0,0 @@
-from models.engine import load_engine
-from utils.utils import read_config
-
-if __name__ == "__main__":
-    CONFIG_PATH = "d4pg-pytorch/configs/follow_rl.yml"
-    config = read_config(CONFIG_PATH)
-
-    engine = load_engine(config)
-    engine.train()
